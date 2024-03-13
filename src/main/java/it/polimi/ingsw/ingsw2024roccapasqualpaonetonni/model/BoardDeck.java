42bbc7a45d88cc0d7826192accd766d128cf807d
Arianna <ariannapaone02@gmail.com>
2024-03-13 16:33:39 +0100

    Deck classes

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/BoardDeck.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/BoardDeck.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
 
+import java.util.Arrays;
+
 public class BoardDeck {
+    private ResourceCard[] resourceCards=new ResourceCard[2];
+    private GoldCard[] goldCards=new GoldCard[2];
+    private ObjectiveCard[] commonGoals=new ObjectiveCard[2];
+    private PlayingCard temp;
+
+    public BoardDeck(ResourceCard[] rc,GoldCard[] gc, ObjectiveCard[] oc){
+        this.resourceCards= Arrays.copyOf(rc,2);
+        this.goldCards= Arrays.copyOf(gc,2);
+        this.commonGoals= Arrays.copyOf(oc,2);
+    }
+    //ci sara un metodo in game che estra 2 carte da ogni mazzo e le mette in un array che poi passa al costruttore della board
+
+    public PlayingCard draw(int position,DrawableDeck d){ //le 4 carte nella board sono chiamate 1,2,3,4, passando la position il client puo decidere quale carta vuole percare tra le 4, quindi indirettamente nache il suo tipo
+        if(position==1 || position==2){
+            temp=resourceCards[position-1];
+            resourceCards[position-1]=d.drawFirstResource();
+            return temp;
+        }
+        else {
+            temp=goldCards[position-3];
+            goldCards[position-1]=d.drawFirstGold();
+            return temp;
+        }
+    }
 }
--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/DrawableDeck.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/DrawableDeck.java
@@ -1,4 +1,36 @@
 package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class DrawableDeck {
+    private Queue<GoldCard> goldQueue=new LinkedList<>();
+    private Queue<ResourceCard> resourceQueue=new LinkedList<>();
+    private Queue<ObjectiveCard> objectiveQueue=new LinkedList<>();
+    private Queue<StartingCard> startingQueue=new LinkedList<>();
+
+    public DrawableDeck(Collection<GoldCard>gc,Collection<ResourceCard>rc,Collection<ObjectiveCard>oc,Collection<StartingCard>sc){
+        this.goldQueue.addAll(gc);
+        this.resourceQueue.addAll(rc);
+        this.objectiveQueue.addAll(oc);
+        this.startingQueue.addAll(sc);
+    }
+    public ResourceCard drawFirstResource()
+    {
+        return resourceQueue.poll();
+    }
+    public GoldCard drawFirstGold()
+    {
+        return goldQueue.poll();
+    }
+    public ObjectiveCard drawFirstObjective()
+    {
+        return objectiveQueue.poll();
+    }
+    public StartingCard drawFirstStarting()
+    {
+        return startingQueue.poll();
+    }
 }
