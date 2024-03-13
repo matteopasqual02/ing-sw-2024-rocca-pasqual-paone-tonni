5226b6bbd851db95b522de0ccad962eaad7bf37a
Arianna <ariannapaone02@gmail.com>
2024-03-13 14:33:23 +0100

    Corner class

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/Corner.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/Corner.java
@@ -1,4 +1,28 @@
 package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
 
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.exception.alreadyCoveredException;
+
 public class Corner {
+    private int position;
+    private Seed seed;
+    Boolean isFree;
+
+    public Corner(int position,Seed seed){
+        this.position=position;
+        this.seed=seed;
+        isFree=true;
+    }
+
+    public void cover(int position) throws alreadyCoveredException {
+        if(isFree=true){
+            isFree=false;
+        }
+        else {
+            throw new alreadyCoveredException("already covered");
+        }
+    }
+    public boolean isCovered(int position){
+        if(isFree=true) return true;
+        return false;
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/Seed.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
+
+public enum Seed {
+    EMPTY,GREEN,BLUE,RED,PURPLE,FEATHER,POTION,SCROLL
+}
