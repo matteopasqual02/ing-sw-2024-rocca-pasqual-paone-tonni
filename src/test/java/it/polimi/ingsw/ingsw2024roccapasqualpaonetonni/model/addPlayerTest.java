15939335ea816cfa348c3cffb6cec7086d531f29
Arianna <ariannapaone02@gmail.com>
2024-03-25 20:13:26 +0100

    creazione classe di test

---
+++ src/test/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/addPlayerTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class addPlayerTest {
+
+    @Test
+    void addPlayer() {
+    }
+}
