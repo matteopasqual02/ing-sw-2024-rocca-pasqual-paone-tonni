5843496fdbf24363cf9ed2ff998b992f685c0428
Arianna <ariannapaone02@gmail.com>
2024-03-26 10:22:23 +0100

    creazione classe di test addPlayer

--- src/test/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/addPlayerTest.java
+++ src/test/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/addPlayerTest.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
 
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.exception.GameAlreadyFullException;
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.exception.PlayerAlreadyInException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class addPlayerTest {
 
     @Test
-    void addPlayer() {
+    void addPlayerDimention0() { //test che valuta l'aggiunta con dimensione 0 iniziale
+        Game game= new Game();
+        Player p=new Player("giocatore",1);
+        assertThrows(GameAlreadyFullException.class,
+                ()->{
+            game.addPlayer(p);
+                });
     }
-}
+    @Test
+    //valuta che non si puo aggiungere 2 volte lo stesso player
+    void addSamePlayer() throws GameAlreadyFullException, PlayerAlreadyInException {
+        Game game=new Game();
+        Player p1= new Player("p",1);
+        game.setNumberOfPlayer(2);
+        game.addPlayer(p1);
+        assertThrows(PlayerAlreadyInException.class,
+                ()->{
+            game.addPlayer(p1);
+                });
+
+    }
+    @Test
+    void addTooManyPlayers() throws GameAlreadyFullException, PlayerAlreadyInException {
+        Game game=new Game();
+        Player p1= new Player("p",1);
+        Player p2= new Player("p2",2);
+        game.setNumberOfPlayer(1);
+        game.addPlayer(p1);
+        assertThrows(GameAlreadyFullException.class,
+                ()->{
+                    game.addPlayer(p2);
+                });
+    }
+    }
