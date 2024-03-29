9bd5032e35870e14f6e54d8d718a0f5415c3b603
Arianna <ariannapaone02@gmail.com>
2024-03-29 09:44:37 +0100

    Test that does not pass putting the card on a null corner, we have to make some changes to handle the null corner

--- src/test/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/PlayerBoardTest.java
+++ src/test/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/PlayerBoardTest.java
@@ -4,6 +4,7 @@ import static it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.Seed.EMPTY;
 import static it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.Seed.GREEN;
 import static org.junit.jupiter.api.Assertions.*;
 
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.exception.GameAlreadyFullException;
 import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.exception.InvalidPlaceException;
 import org.junit.jupiter.api.Test;
 class PlayerBoardTest {
@@ -207,6 +208,43 @@ class PlayerBoardTest {
         int[] coordinates = card_to_add.getCoordinates();
         int[] twenty={21,19};
         assertArrayEquals(twenty,coordinates);
+
+
+    }
+    @Test
+    void addResourceCardOnNullCornerTest() throws InvalidPlaceException {
+
+        //starting card placed in the middle
+        Boolean[] c = {true,true,false,false};
+        Corner cf1 = new Corner(1,EMPTY);
+        Corner cf2 = new Corner(2,GREEN);
+        Corner cf3 = new Corner(3,null);
+        Corner cf4 = new Corner(4,EMPTY);
+        Corner cb1 = new Corner(1,EMPTY);
+        Corner cb2 = new Corner(2,EMPTY);
+        Corner cb3 = new Corner(3,EMPTY);
+        Corner cb4 = new Corner(4,EMPTY);
+        Corner[] cf ={cf1,cf2,cf3,cf4};
+        Corner[] cb ={cb1,cb2,cb3,cb4};
+        StartingCard cardToAdd=new StartingCard(1,c,cf,cb);
+
+        //resource card to be added on it
+        Corner c1 = new Corner(1,EMPTY);
+        Corner c2 = new Corner(2,GREEN);
+        Corner c3 = new Corner(3,GREEN);
+        Corner c4 = new Corner(4,EMPTY);
+        Corner[] cc ={c1,c2,c3,c4};
+        ResourceCard card_to_add = new ResourceCard(10,GREEN,cc,1);
+
+        //putting starting card in the middle
+        Player owner=new Player("a",1);
+        PlayerBoard board1 = new PlayerBoard(owner);
+        board1.addStartingCard(cardToAdd);
+
+        assertThrows(InvalidPlaceException.class,
+                ()->{
+                    board1.addCard(card_to_add,cardToAdd,3,owner.getCountSeed());
+                });
 
 
     }
