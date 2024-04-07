731fef3277d1f77de73174477afa86dc18b0ffa9
Federico Rocca <fede.rock.2002@gmail.com>
2024-04-07 22:53:35 +0200

    Fixed PlayerBoard dimensions update on increase

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/PlayerBoard.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/PlayerBoard.java
@@ -63,7 +63,9 @@ public class PlayerBoard {
         int row_offset = 0;
         int col_offset = 0;
         PlayingCard[][] tmp_board = board;
-        board = new PlayingCard[dim_x + Math.abs(x_increase)][dim_y + Math.abs(y_increase)];
+        dim_x = dim_x + Math.abs(x_increase);
+        dim_y = dim_y + Math.abs(y_increase);
+        board = new PlayingCard[dim_x][dim_y];
 
         if (x_increase < 0) {
             row_offset = - x_increase;
