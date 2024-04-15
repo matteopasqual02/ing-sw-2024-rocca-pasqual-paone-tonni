9cee2d1cefcba533a22c9164a57fc74043c07548
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-04-15 17:23:49 +0200

    GameImmutable.java started

---
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/immutable/GameImmutable.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.immutable;
+
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.BoardDeck;
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.DrawableDeck;
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.GameStatus;
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.Player;
+import it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.chat.Chat;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+public class GameImmutable {
+
+
+
+
+}
