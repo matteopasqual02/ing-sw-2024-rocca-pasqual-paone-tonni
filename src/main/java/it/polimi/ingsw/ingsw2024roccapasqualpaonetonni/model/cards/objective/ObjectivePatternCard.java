355230b51f5b14a66b76b97427a47799f5447236
alberto <tonni.alberto@gmail.com>
2024-05-03 18:44:35 +0200

    toString Resource, Gold, Starting, Ovjective finished

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/cards/objective/ObjectivePatternCard.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/model/cards/objective/ObjectivePatternCard.java
@@ -40,21 +40,24 @@ public class ObjectivePatternCard extends ObjectiveCard {
                     if(pattern[i][j]==null){
                         sb.append(ansi().cursor(0,0).fg(Ansi.Color.DEFAULT).bg(background).a(" "));
                     }else {
+                        sb.append(ansi().cursor(0,0).fg(Ansi.Color.DEFAULT).bg(Ansi.Color.DEFAULT).a(" "));
                         sb.append(ansi().cursor(0,0).fg(pattern[i][j].getByAnsi()).bg(Ansi.Color.DEFAULT).a(pattern[i][j].name().substring(0,1)));
-
+                        sb.append(ansi().cursor(0,0).fg(Ansi.Color.DEFAULT).bg(Ansi.Color.DEFAULT).a(" "));
                     }
                 }
                 sb.append(ansi().cursor(0,0).fg(background).bg(background).a("  "));
                 if(line == 0){
-                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("        "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("       "));
                 }
                 else if(line == 1){
-                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("    "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("  "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(Ansi.Color.DEFAULT).a(" "));
                     sb.append(ansi().cursor(0,0).fg(Ansi.Color.DEFAULT).bg(Ansi.Color.DEFAULT).a(points));
-                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("   "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(Ansi.Color.DEFAULT).a(" "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("  "));
                 }
                 else if(line == 2){
-                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("        "));
+                    sb.append(ansi().cursor(0,0).fg(background).bg(background).a("       "));
                 }
                 if(count!=0){
                     sb.append(ansi().cursor(0,0).fg(Ansi.Color.DEFAULT).bg(Ansi.Color.DEFAULT).a("\n"));
