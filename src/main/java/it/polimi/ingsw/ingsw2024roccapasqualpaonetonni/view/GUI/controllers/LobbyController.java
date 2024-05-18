2258d8ab8551c626432884fa068dfe8ce379027a
alberto <tonni.alberto@gmail.com>
2024-05-18 11:15:48 +0200

    Creation file NameGameId_1.fxml (JoinGameID page)

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/GUI/controllers/LobbyController.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/GUI/controllers/LobbyController.java
@@ -49,7 +49,7 @@ public class LobbyController extends GenericController {
     public void handleJoinGameIDButtonClick(ActionEvent event){
         Platform.runLater(()-> {
             try {
-                application.changeScene("/NameGameId.fxml");
+                application.changeScene("/NameGameId_1.fxml");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
