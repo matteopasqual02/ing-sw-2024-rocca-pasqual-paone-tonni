8b6c3786a3adff4231598dfbe99eaff10d7eb785
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-05-21 15:36:10 +0200

    refactor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,6 +13,12 @@ module it.polimi.ingsw.ingsw2024roccapasqualpaonetonni {
     //I had to add this last one to make mainServer work
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.controller.controllerInterface;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.listener;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.chat;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.immutable;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.cards;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.cards.objective;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI;
 }
