e9c3b2af41292758b71695914208abe7994fc198
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-05-22 11:43:44 +0200

    bug resolved

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/GUI/controllers/JoinedGameController.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/GUI/controllers/JoinedGameController.java
@@ -5,10 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class JoinedGameController {
-    @FXML
-    private VBox labelBox;
+
     public void addNewLabel(String text){
-        Label newLabel = new Label(text);
-        labelBox.getChildren().add(newLabel);
+        new Label(text);
     }
 }
