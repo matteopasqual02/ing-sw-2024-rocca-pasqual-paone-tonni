0a4ab34d6a084c76519ca7b836000c63932915f1
teopa <matteoromilio.pasqual@mail.polimi.it>
2024-06-19 12:34:50 +0200

    testing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module it.polimi.ingsw.ingsw2024roccapasqualpaonetonni {
     requires java.rmi;
 
     opens it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI to javafx.fxml;
+    opens it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI.controllers to javafx.fxml;
 
     //I had to add this last one to make mainServer work
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.controller.controllerInterface;
@@ -19,8 +20,16 @@ module it.polimi.ingsw.ingsw2024roccapasqualpaonetonni {
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.cards;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.model.cards.objective;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.main;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.RMI;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.socket.client;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.socket.server;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.socket.clientMessages;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.network.socket.serverMessages;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.utils;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view;
+    exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.TUI;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI;
     exports it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI.controllers;
-    opens it.polimi.ingsw.ingsw2024roccapasqualpaonetonni.view.GUI.controllers to javafx.fxml;
+
 }
