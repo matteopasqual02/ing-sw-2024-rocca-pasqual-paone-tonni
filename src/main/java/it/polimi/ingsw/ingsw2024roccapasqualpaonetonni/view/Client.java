ec6a0f45885c7c3db909e04e36f8bc58c83a0350
Arianna <arianna.paone@mail.polimi.it>
2024-06-27 23:18:27 +0200

    thread interrupt bug fixed

--- src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/Client.java
+++ src/main/java/it/polimi/ingsw/ingsw2024roccapasqualpaonetonni/view/Client.java
@@ -879,7 +879,6 @@ public class Client extends UnicastRemoteObject implements GameListener{
                 synchronized (lock) {
                     if (!pinged) {
                         ConsolePrinter.consolePrinter("[ERROR]: server dead game interrupted");
-                        this.interrupt();
                         System.exit(0);
                     } else {
                         pinged = false;
